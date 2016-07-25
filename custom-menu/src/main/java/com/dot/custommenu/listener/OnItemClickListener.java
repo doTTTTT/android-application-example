a5976acd62166f1af6bf6d9f31b71947fa5fdb95
teyssa_r <raphael.teyssandier@epitech.eu>
2016-07-25 13:37:08 +0200

    Initial Commit

--- custom-menu/src/main/java/com/dot/custommenu/listener/OnItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.dot.custommenu.listener;
-
-
-import com.dot.custommenu.model.ListItemModel;
-
-public interface OnItemClickListener {
-    public void onClick(ListItemModel item);
-}
--- custom-menu/src/main/java/com/dot/custommenu/listener/OnMenuItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.dot.custommenu.listener;
-
-import com.dot.custommenu.model.ListItemModel;
-
-public interface OnMenuItemClickListener {
-    public void onMenuItemSelected(ListItemModel position);
-}
